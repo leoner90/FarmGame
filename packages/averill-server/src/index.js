2fffad8c913dfea0a8597d39741cf0445eb97964
Your Name <you@example.com>
2021-11-21 16:44:21 +0200

    test

--- packages/averill-server/src/index.js
+++ packages/averill-server/src/index.js
@@ -23,6 +23,4 @@ app.use(Router)
 
 // //START SERVER LISTENER
 const port = 3000
-app.listen(port, function () {
-  // console.log(`Server running at http://localhost:${port}`);
-})
+app.listen(port, function () {})
